c8611531b3950ac6fdc9028ce79ca72085423524
Thiago Rose <thiago.senhorinha@gmail.com>
2014-04-04 10:52:44 -0300

    Criando loop principal e ascii art

--- src/model/Interpretador.cpp
+++ src/model/Interpretador.cpp
@@ -6,10 +6,23 @@
 using namespace std;
 
 int main() {
-
 	TerminalColorido *t = new TerminalColorido();
 	t->limpar();
-	t->parteInicialDoTerminal();
-	t->mensagemDeAlerta("ALERTA!!!!");
 
+	// Imprime "Interpretador" na cor verde
+	// Arte feita por http://patorjk.com/software/taag/#p=display&f=Ogre&t=Interpretador
+	t->mensagemDeSucesso("  _____       _                           _            _      			");
+	t->mensagemDeSucesso("  \\_   \\_ __ | |_ ___ _ __ _ __  _ __ ___| |_ __ _  __| | ___  _ __ 	");
+	t->mensagemDeSucesso("   / /\\/ '_ \\| __/ _ \\ '__| '_ \\| '__/ _ \\ __/ _` |/ _` |/ _ \\| '__|	");
+	t->mensagemDeSucesso("/\\/ /_ | | | | ||  __/ |  | |_) | | |  __/ || (_| | (_| | (_) | |   	" );
+	t->mensagemDeSucesso("\\____/ |_| |_|\\__\\___|_|  | .__/|_|  \\___|\\__\\__,_|\\__,_|\\___/|_|   	");
+	t->mensagemDeSucesso("                          |_|                                       	");
+
+
+
+	while(true) {
+		string comando;
+		t->parteInicialDoTerminal();
+		cin >> comando;
+	}
 }
