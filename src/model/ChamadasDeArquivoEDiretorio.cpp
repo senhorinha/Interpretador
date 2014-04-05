e5cb892ab08145c3ae47ab606ac11c02c2a5f287
Thiago Rose <thiago.senhorinha@gmail.com>
2014-04-05 12:26:40 -0300

    Implementando uso do namegaurd

--- src/model/ChamadasDeArquivoEDiretorio.cpp
+++ src/model/ChamadasDeArquivoEDiretorio.cpp
@@ -1,35 +1,58 @@
 #include "ChamadasDeArquivoEDiretorio.h"
 
-void ChamadasDeArquivoEDiretorio::access_teste(const char *pathname, int mode) {
+ChamadasDeArquivoEDiretorio::ChamadasDeArquivoEDiretorio() {
+
+}
+
+bool ChamadasDeArquivoEDiretorio::verificarPermissoes(string permissoes,
+		string caminho) {
 	int resultado;
-	resultado = access(pathname, mode);
+	int mode;
+	if (permissoes.size() <= 3) {
+		if (permissoes.find("r")) {
+			mode += R_OK;
+		}
+		if (permissoes.find("w")) {
+			mode += W_OK;
+		}
+		if (permissoes.find("x")) {
+			mode += X_OK;
+		}
+	}
+	char* path = strdup(caminho.c_str());
+	resultado = access(path, mode);
 	if (resultado == 0) {
-		terminal->mensagemDeSucesso("Sucesso! Você possui permissão!");
+		return true;
 	} else {
-		terminal->mensagemDeErro("Erro! Você não possui permissão!");
+		return false;
 	}
 }
 
-void ChamadasDeArquivoEDiretorio::chmod(const char *path, mode_t mode) {
-	int status;
+bool ChamadasDeArquivoEDiretorio::alterarPermissoes(string caminho,
+		mode_t mode) {
 	struct stat buffer;
+	char* path = strdup(caminho.c_str());
 	chmod(path, mode);
-	status = stat(path, &buffer);
+	int status = stat(path, &buffer);
 	if (status == 0) {
-		terminal->mensagemDeSucesso("Sucesso! Permissão alterada.");
+		return true;
 	} else {
-		terminal->mensagemDeErro("Erro! Permissão não alterada!");
+		return false;
 	}
 }
 
-void ChamadasDeArquivoEDiretorio::rename_teste(const char *oldpath,
-		const char *newpath) {
-	int resultado;
-	resultado = rename(oldpath, newpath);
+bool ChamadasDeArquivoEDiretorio::renomearArquivo(string caminhoAntigo,
+		string caminhoNovo) {
+	char* velho = strdup(caminhoAntigo.c_str());
+	char* novo = strdup(caminhoNovo.c_str());
+	int resultado = rename(velho, novo);
+	delete[] velho;
+	delete[] novo;
 	if (resultado == 0) {
-		terminal->mensagemDeSucesso("Sucesso! Arquivo renomeado!");
+		return true;
 	} else {
-		terminal->mensagemDeErro("Erro! Arquivo não renomeado!");
+		return false;
 	}
-
 }
+;
+
--- src/model/ChamadasDeArquivoEDiretorio.h
+++ src/model/ChamadasDeArquivoEDiretorio.h
@@ -1,20 +1,30 @@
-#include "../view/Terminal.h"
+/*
+ * ChamadasDeArquivoEDiretorio.h
+ *
+ *  Created on: 05/04/2014
+ *      Author: thiago
+ */
+
+#ifndef CHAMADASDEARQUIVOEDIRETORIO_H_
+#define CHAMADASDEARQUIVOEDIRETORIO_H_
+
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
+using namespace std;
 
 class ChamadasDeArquivoEDiretorio {
-	Terminal * terminal;
 
 public:
-	ChamadasDeArquivoEDiretorio(Terminal *t) {
-		terminal = t;
-	}
-	void access_teste(const char *pathname, int mode);
-	void chmod(const char *path, mode_t mode);
-	void rename_teste(const char *oldpath, const char *newpath);
-	virtual ~ChamadasDeArquivoEDiretorio();
+	ChamadasDeArquivoEDiretorio();
+	bool verificarPermissoes(string permissoes, string caminho);
+	bool alterarPermissoes(string caminho, mode_t mode);
+	bool renomearArquivo(string caminhoAntigo, string caminhoNovo);
 };
 
+
+
+#endif /* CHAMADASDEARQUIVOEDIRETORIO_H_ */
